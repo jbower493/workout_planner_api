1a5326fb92df2c835c728eee9423168150a45fc8
Jamie Bower <j_bower@hotmail.co.uk>
2020-10-29 14:52:12 +1100

    made all responses consistent sending json with a success property and either user, workouts, exercises or error property

--- server.js
+++ server.js
@@ -66,13 +66,12 @@ app.post('/new-exercise', (req, res, next) => {
 
   pressUps.save()
     .then(doc => {
-      console.log(doc)
-      res.send({
+      res.json({
         success: true,
-        exercise: doc
+        exercises: req.user.exercises
       });
     })
-    .catch(e => res.send({ success: false }));
+    .catch(e => next(e));
 });
 
 app.post('/edit-exercise/:exerciseId', (req, res, next) => {
@@ -81,11 +80,11 @@ app.post('/edit-exercise/:exerciseId', (req, res, next) => {
     description: req.body.description,
     muscleGroup: req.body.muscleGroup
   })
-    .then(doc => res.send({ success: true }))
-    .catch(e => {
-      console.log(e);
-      res.send({ success: false });
-    })
+    .then(doc => res.json({
+      success: true,
+      exercises: req.user.exercises
+    }))
+    .catch(e => next(e))
 });
 
 app.post('/new-workout', (req, res, next) => {
@@ -99,31 +98,33 @@ app.post('/new-workout', (req, res, next) => {
 
   currentUser.workouts.push(workout);
   currentUser.save()
-    .then(doc => {
-      console.log(doc)
-      res.send({ success: true });
-    })
-    .catch(e => res.send({ success: false }));
+    .then(doc => res.json({
+      success: true,
+      workouts: req.user.workouts
+    }))
+    .catch(e => next(e))
 });
 
 app.get('/get-workouts', (req, res, next) => {
-  //res.send({ workouts: req.user.workouts });
   User.findById(req.user.id)
     .populate({
       path: 'workouts.exercises.exercise',
       model: 'Exercise'
     })
-    .then(doc => {
-      res.send({ workouts: doc.workouts })
-    })
-    .catch(e => res.send({ success: false }))
+    .then(doc => res.json({
+      success: true,
+      workouts: doc.workouts
+    }))
+    .catch(e => next(e))
 });
 
 app.get('/get-exercises', (req, res, next) => {
   Exercise.find({ owner: req.user.id })
-    .then(docs => {
-      res.send({ exercises: docs });
-    })
+    .then(docs => res.json({
+      success: true,
+      exercises: docs
+    }))
+    .catch(e => next(e))
 });
 
 app.post('/add-to-workout/:workoutId', (req, res, next) => {
@@ -141,11 +142,11 @@ app.post('/add-to-workout/:workoutId', (req, res, next) => {
   workoutsArr[index] = currentWorkout;
   // update user, set workouts as updated workouts array
   User.findByIdAndUpdate(req.user.id, { workouts: workoutsArr })
-    .then(doc => {
-      res.send({ success: true });
-      console.log(doc)
-    })
-    .catch(e => res.send({ success: false }))
+    .then(doc => res.json({
+      success: true,
+      workouts: req.user.workouts
+    }))
+    .catch(e => next(e))
 });
 
 app.delete('/workout/:workoutId', (req, res, next) => {
@@ -159,11 +160,11 @@ app.delete('/workout/:workoutId', (req, res, next) => {
   workoutsArr.splice(index, 1);
   // update user, set workouts as updated workouts array
   User.findByIdAndUpdate(req.user.id, { workouts: workoutsArr })
-    .then(doc => {
-      res.send({ success: true });
-      console.log(doc)
-    })
-    .catch(e => res.send({ success: false }))
+    .then(doc => res.json({
+      success: true,
+      workouts: req.user.workouts
+    }))
+    .catch(e => next(e))
 });
 
 app.delete('/workout-exercise/:workoutExerciseId/:workoutId', (req, res, next) => {
@@ -174,11 +175,11 @@ app.delete('/workout-exercise/:workoutExerciseId/:workoutId', (req, res, next) =
   const exerciseIndex = workout.exercises.indexOf(workoutExercise);
   workouts[workoutIndex].exercises.splice(exerciseIndex, 1);
   User.findByIdAndUpdate(req.user.id, { workouts })
-    .then(doc => {
-      res.send({ success: true });
-      console.log(doc)
-    })
-    .catch(e => res.send({ success: false }))
+    .then(doc => res.json({
+      success: true,
+      workouts: req.user.workouts
+    }))
+    .catch(e => next(e))
 });
 
 app.delete('/exercise/:exerciseId', (req, res, next) => {
@@ -193,9 +194,13 @@ app.delete('/exercise/:exerciseId', (req, res, next) => {
         }
       })
       User.findByIdAndUpdate(req.user.id, { workouts })
-        .then(result => res.send({ success: true }));
+        .then(result => res.json({
+          success: true,
+          workouts: req.user.workouts,
+          exercises: req.user.exercises
+        }));
     })
-    .catch(e => res.send({ success: false }));
+    .catch(e => next(e));
 });
 
 app.post('/edit-workout-exercise/:workoutId/:workoutExerciseId', (req, res, next) => {
@@ -206,11 +211,11 @@ app.post('/edit-workout-exercise/:workoutId/:workoutExerciseId', (req, res, next
   workoutExercise.sets = req.body.sets;
   workoutExercise.weight = req.body.weight;
   User.findByIdAndUpdate(req.user._id, { workouts })
-    .then(doc => res.send({ success: true }))
-    .catch(e => {
-      res.send({ success: false });
-      console.log(e)
-    });
+    .then(doc => res.json({
+      success: true,
+      workouts: req.user.workouts
+    }))
+    .catch(e => next(e))
 });
 
 app.post('/edit-workout/:workoutId', (req, res, next) => {
@@ -220,38 +225,66 @@ app.post('/edit-workout/:workoutId', (req, res, next) => {
   workout.duration = req.body.duration;
   workout.type = req.body.type;
   User.findByIdAndUpdate(req.user._id, { workouts })
-    .then(doc => res.send({ success: true }))
-    .catch(e => {
-      res.send({ success: false });
-      console.log(e)
-    });
+    .then(doc => res.json({
+      success: true,
+      workouts: req.user.workouts
+    }))
+    .catch(e => next(e))
 });
 
+// auth routes
+/*
 app.post('/register', (req, res, next) => {
   User.findOne({ username: req.body.username }, async (err, doc) => {
     if(err) throw err;
-    if(doc) return res.send({ message: 'User already exists' });
+    if(doc) return res.json({
+      success: false,
+      error: 'User already exists'
+    });
     const hash = await bcrypt.hash(req.body.password, 10);
     const newUser = new User({
       username: req.body.username,
       password: hash
     });
     await newUser.save();
-    res.send({ message: 'New user created' });
+    res.json({ message: 'New user created' });
   });
+});*/
+
+app.post('/register', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if(user) return res.json({
+      success: false,
+      error: 'User already exists'
+    });
+    const hash = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+      username: req.body.username,
+      password: hash
+    });
+    await newUser.save();
+    res.json({
+      success: true
+    });
+  } catch(e) {
+    next(e);
+  }
 });
 
 app.post('/login', (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
-    if(err) throw err;
-    if(!user) return res.send({ message: 'No user exists' });
+    if(err) return next(err);
+    if(!user) return res.json({
+      success: false,
+      error: 'Incorrect username and password combination'
+    });
     req.login(user, err => {
-      if(err) throw err;
-      res.send({
-        message: 'Successfully authenticated',
+      if(err) return next(err);
+      res.json({
+        success: true,
         user: req.user
       });
-      console.log(req.user);
     });
   })(req, res, next);
 });
@@ -259,19 +292,29 @@ app.post('/login', (req, res, next) => {
 app.get('/logout', (req, res, next) => {
   if(req.user) {
     req.logout();
-    console.log(req.user);
-    res.send({ message: 'Successfully logged out' });
+    res.json({ success: true });
   } else {
-    res.send({ message: 'Couldn\'t logout, no logged in user' });
+    res.json({ success: false });
   }
 });
 
 app.get('/get-user', (req, res, next) => {
   if(!req.user) {
-    return res.send({ message: 'No user logged in' })
+    return res.json({ success: false });
   }
-  console.log(req.user);
-  res.send({ user: req.user });
+  res.json({
+    success: true,
+    user: req.user
+  });
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.json({
+    success: false,
+    error: 'Server error, apologies.'
+  });
 });
 
 
